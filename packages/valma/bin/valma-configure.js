fd49541f7add3dfce2a66695a4c622429d206bbf
iridian <iridian@iki.fi>
2018-06-10 15:01:38 +0300

    Adds valma-configure:vlm.askToCreateValmaScriptSkeleton

--- packages/valma/bin/valma-configure.js
+++ packages/valma/bin/valma-configure.js
@@ -22,9 +22,53 @@ exports.handler = async (yargv) => {
         + "package.json does not have valaa section, or valaa.type or valaa.domain settings"
         + "(maybe run 'vlm init'?)");
   }
+  vlm.askToCreateValmaScriptSkeleton = askToCreateValmaScriptSkeleton;
   if (!yargv.moduleglob) {
     await vlm.callValma(`.configure/.modules`, yargv._.slice(1));
   }
   await vlm.callValma(`.configure/{,.type/.${valaa.type}/,.domain/.${valaa.type}/}.module/${
       yargv.moduleglob || ""}{*/**/,}*`);
+
+  async function askToCreateValmaScriptSkeleton (script, scriptFile, briefText, summaryText,
+        describeText) {
+    const underscoredScript = script.replace(/\//g, "_");
+    const command = script.replace("valma-", "");
+    const scriptPath = `bin/${scriptFile}`;
+    let verb = "already exports";
+    while (!(vlm.packageConfig.bin || {})[underscoredScript]) {
+      const choices = ["Create", "skip"];
+      if (describeText) choices.push("help");
+      const answer = await vlm.inquire([{
+        message: `Create a ${briefText} valma script template as package.json:bin["${
+            underscoredScript}"] -> "${scriptPath}"?`,
+        type: "list", name: "choice", default: choices[0], choices,
+      }]);
+      if (answer.choice === "skip") {
+        verb = "still doesn't export";
+        break;
+      }
+      if (answer.choice === "help") {
+        console.log(describeText);
+        console.log(`This step creates a ${briefText} script template for this component\n`);
+        continue;
+      }
+      shell.mkdir("-p", "bin");
+      shell.ShellString(
+`exports.command = "${command}";
+exports.summary = "${summaryText || ""}";
+exports.describe = \`\${exports.summary}.\n${describeText || ""}\`;
+
+exports.builder = (yargs) => yargs;
+
+exports.handler = (yargv) => {
+  const vlm = yargv.vlm;
+};
+`).to(scriptPath);
+      vlm.updatePackageConfig({ bin: { [underscoredScript]: scriptPath } });
+      verb = "now exports";
+    }
+    if (vlm.verbosity >= 1) {
+      console.log(`valma-configure inform: repository ${verb} valma command ${command}`);
+    }
+  }
 };
