09e8adecc22c06132466370eb42de46a7f19af8f
iridian <iridian@iki.fi>
2018-06-26 23:28:56 +0300

    Adds @valos/toolset-vault-operations package skeleton

---
+++ packages/toolset-vault-operations/valma/configure__vault_toolset__toolset-vault-operations.js
@@ -0,0 +1,16 @@
+exports.command = ".configure/.type/.vault/.toolset/@valos/toolset-vault-operations";
+exports.summary = "Configure the toolset 'toolset-vault-operations' for the current vault";
+exports.describe = `${exports.summary}.
+This script makes the toolset 'toolset-vault-operations' available for
+grabbing by repositories with valaa type 'vault'.`;
+
+exports.disabled = (yargs) => !yargs.vlm.packageConfig;
+exports.builder = (yargs) => {
+  const vlm = yargs.vlm;
+  return yargs;
+};
+
+exports.handler = (yargv) => {
+  const vlm = yargv.vlm;
+  return true;
+};
